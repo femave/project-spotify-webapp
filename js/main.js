90139d60ddc5e39524dd498cfed461361dc1e6f7
Marc Vergara <marc.vergara.f@gmail.com>
2017-07-19 21:54:14 +0200

    Improveds and all functional no styles work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
   // Use your own token (this is just an example)
-  var token = 'BQAsGg1C5fHOp1Ka7y1I84-RhiNNtG_e0V3tlspys7vPM7ikyKjwTidq-zITuHHA3v-Fx6XYxlRJ4q5xVAkgBbtyUzyfdlOcr-oASFlIwiR6dW83sbCXLHuiurzuGThhOurLJx272Vw'
+  var token = 'BQDZ1njih0LAUoeVZNH1_FAlkyUF8vCTay2EOdleJqUWrb9mvT0LuiI6hsrqv1F-_ktQPxQBaiRONzIcSHHDwn0bBEVfGIyg5cdEPSWrBUizUOiNVGtIdUNc34TLBoayzBz-x34MSvE'
 
   $('button').on('click', function (e) {
     e.preventDefault()
@@ -12,19 +12,20 @@
       }
     })
 
-    .then(function (oData) {
-      var aOptionsArtist = oData.artists.items.map(function (artist) {
-        // console.log(artist.name)
-        return '<option value="' + artist.id + '">' + artist.name + '</option>'
-      })
+          .then(function (oData) {
+            var aOptionsArtist = oData.artists.items.map(function (artist) {
+                  // console.log(artist.name)
+              return '<option value="' + artist.id + '">' + artist.name + '</option>'
+            })
 
-      htmlSelect = aOptionsArtist.join('')
-      $('#listArtist').html(htmlSelect)
-    })
+            $('#listArtist').removeClass('hidden')
+            htmlSelect = aOptionsArtist.join('')
+            $('#listArtist').html('<option disabled selected>Select an artist</option>' + htmlSelect)
+          })
 
     $('select').on('change', function (artists) {
       var inputSelect = $(this).val()
-      // console.log(inputSelect)
+          // console.log(inputSelect)
 
       $.ajax({
         url: 'https://api.spotify.com/v1/artists/' + inputSelect + '/albums',
@@ -33,41 +34,28 @@
         }
       })
 
-      .then(function (oData) {
-        console.log(oData)
-        var aOptionsAlbums = oData.items.map(function (artist) {
-          console.log(artist.id)
-          var albumId = artist.id
-          var albumUrl = artist.url
-          var albumImg = artist.images[1].url
+              .then(function (oData) {
+                // console.log(oData)
+                var aOptionsAlbums = oData.items.map(function (artist) {
+                  // console.log(artist.id)
+                  var albumId = artist.id
+                  var albumUrl = artist.url
+                  var albumImg = artist.images[1].url
 
-          // $.ajax({
-          //   url: 'https://api.spotify.com/v1/albums/' + albumId + '/tracks',
-          //   headers: {
-          //     Authorization: 'Bearer ' + token
-          //   }
-          // })
+                  return '<div class="col-md-3"><div class="thumbnail"><img value="' + artist.name + '" id="' + albumId + '" class="img-responsive" src="' + albumImg + '"><div class="caption"><h6>"' + artist.name + '"</h6></div></div></div>'
+                })
 
-          // .then(function (oData) {
-          //   console.log(oData)
-          //   var songs = oData.items.map(function (song) {
-          //     console.log(song.name)
-          //     song += '<li>' + song.name + '</li>'
-          //   })
-          // })
-
-          return '<div class="col-md-4"><div class="thumbnail"><img id="' + albumId + '" class="img-responsive" src="' + albumImg + '"><div class="caption"><h6>"' + artist.name + '"</h6></div></div></div>'
-        })
-
-        var htmlSelect = aOptionsAlbums
-        $('#artistThumbnail').html(htmlSelect)
-      })
+                var htmlSelect = aOptionsAlbums
+                $('#artistThumbnail').html(htmlSelect)
+              })
     })
 
     $('#artistThumbnail').on('click', 'img', function (song) {
       e.preventDefault()
       var albumId = $(this).attr('id')
-      console.log(albumId)
+      var songListTitle = $(this).attr('value')
+      console.log(songListTitle)
+      // console.log(albumId)
       $.ajax({
         url: 'https://api.spotify.com/v1/albums/' + albumId + '/tracks',
         headers: {
@@ -75,16 +63,26 @@
         }
       })
 
-          .then(function (oData) {
-            console.log(oData)
-            var aOptionsSongs = oData.items.map(function (song) {
-              console.log(song.name)
-              var songPreview = song.preview_url
-              return '<div><li><a target="_blank" href="' + songPreview + '">' + song.name + '</a></li></div>'
-            })
+              .then(function (oData) {
+                // console.log(oData)
+                var aOptionsSongs = oData.items.map(function (song) {
+                  // console.log(song.name)
+                  var songPreview = song.preview_url
+                  return '<div><li><a value="' + song.name + '" id="' + songPreview + '">' + song.name + '</a></li></div>'
+                })
 
-            htmlSelect = aOptionsSongs.join('')
-            $('#songList').html(htmlSelect)
-          })
+                $('#songList').removeClass('hidden')
+                htmlSelect = aOptionsSongs.join('')
+                $('#songList').html('<h4>' + songListTitle + '</h4>' + htmlSelect + '<div class="embed" id="embed"></div>')
+              })
+    })
+
+    $('#songList').on('click', 'a', function (play) {
+      e.preventDefault()
+      debugger
+      var songEmbed = $(this).attr('id')
+      var songName = $(this).attr('value')
+      console.log(songName)
+      $('#embed').html('<p>' + songName + '</p><embed height="50px" width="100px" src="' + songEmbed + '" />')
     })
   })
